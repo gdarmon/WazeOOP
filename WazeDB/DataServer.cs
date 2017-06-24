2d391338ea2714cb94c7d06674caf29e05a55fd4
Gdarmon <gilad.darmon@intel.com>
2017-06-24 18:54:50 +0300

    rest of the files

---
+++ WazeDB/DataServer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WazeClient;
+namespace WazeDB
+{
+
+    // different types, motorcycle, bus, private car....
+    public class DataServer : IDataServer
+    {
+        private List<Road> RecordedRoads { get;private set; }
+        private Dictionary<int, Vehicle> OnlineVechicles { get; private set; }
+
+        public DataServer()
+        {
+            RecordedRoads = new List<Road>();
+            OnlineVechicles = new Dictionary<int, Vehicle>();
+        }
+        public List<Road> GetRoadsAroundLocation(int currentLocation)
+        {
+            var result = new List<Road>();
+            foreach (var road in RecordedRoads)
+            {
+                if (road.IsCloseTo(currentLocation))
+                {
+                    result.Add(road);
+                }
+            }
+            return result;
+        }
+
+        public List<Vehicle> GetVechicalesAroundLocation(int currentLocation)
+        {
+            var result = new List<Vehicle>();
+            foreach (var vechicle in OnlineVechicles.Values)
+            {
+                if (vechicle.IsCloseTo(currentLocation))
+                {
+                    result.Add(vechicle);
+                }
+            }
+            return result;
+        }
+
+        public SuggestedRoute GetSuggestedRoute(int currentLocation, int destination)
+        {
+            //for example run 2 bfs from the destination to the current location and check for an intersection point
+            return new SuggestedRoute();
+        }
+        public void AddCarToServer(int carLicsenceNumber, Vehicle car)
+        {
+            if (!OnlineVechicles.ContainsKey(carLicsenceNumber))
+            {
+                OnlineVechicles.Add(carLicsenceNumber, car);
+            }
+        }
+        public void RemoveCarFromServer (int carLicsenceNumber)
+        {
+            if(OnlineVechicles.ContainsKey(carLicsenceNumber))
+            {
+                OnlineVechicles.Remove(carLicsenceNumber);
+            }
+        }
+    }
+}
---
+++ WazeTests/WazeDesignTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using WazeClient;
+using WazeDB;
+
+namespace WazeTests
+{
+    [TestClass]
+    public class WazeDesignTests
+    {
+        [TestMethod]
+        public void CarTest()
+        {
+            Gps gps = new Gps();
+            Car myCar = new Car(gps);
+            DataServer server = new DataServer();
+            UserMap mapByLocation = new UserMap(myCar server);
+            mapByLocation.SetDestination(104410);
+            Assert.AreEqual(System.Drawing.Color.FromName("Purple"), mapByLocation.SuggestedRoute.RoadColor);
+        }
+    }
+}
